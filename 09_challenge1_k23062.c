a79168573c70f7fd539fffea6c2beab424733e6a
toufu-cell <tatsutatsu-kirito.1316@ezweb.ne.jp>
2023-06-21 17:36:15 +0900

    a

--- 09_challenge1_k23062.c
+++ 09_challenge1_k23062.c
@@ -1,13 +1,33 @@
 #include <stdio.h>
 int main(int argc, const char* argv[]) {
   char ch;
-  int isMinus, answer, inputNumber = 0;
+  int isMinus = 1, answer = 0, inputNumber = 0;
   printf("equiation? ");
-  while ((ch = getchar()) != '=') {
-    if (ch == '-') {
-      inputNumber = 1;
+  while ((ch = getchar()) != '\n') {
+    switch (ch) {
+      case ' ':
+      //' 'のとき何もしない
+      case '=':
+        // =のとき何もしない
+        break;
+      case '+':
+        isMinus = 1;  //+のときisMinusに１
+        break;
+      case '-':  //-のときisMinusに０
+        isMinus = 0;
+        break;
+      default:
+        inputNumber = (int)ch - '0';  // chをASCIIコードから-48する
+        if (isMinus == 1) {
+          answer += inputNumber;  //+の処理
+        } else if (isMinus == 0) {
+          answer -= inputNumber;  //-の処理
+        }
+        isMinus = 1;
+        break;
     }
   }
+  printf("answer: %d\n", answer);
 
   return 0;
 }
--- 09_challenge2_k23062.c
+++ 09_challenge2_k23062.c
@@ -1,8 +1,35 @@
 #include <stdio.h>
 int main(int argc, const char* argv[]) {
   char ch;
+  int isMinus = 1, answer = 0, inputNumber = 0;
   printf("equation? ");
-  scanf("%c", &ch);
-  
+  while ((ch = getchar()) != '\n') {
+    if (ch >= '0' && ch <= '9') {
+      inputNumber = inputNumber * 10 + (int)ch - '0';
+    }  // chが0〜9のときchをASCIIから-48する,連続で入力した時入力された数だけ繰り上げる
+    if (inputNumber != 0 && ch == ' ') {
+      switch (isMinus) {
+        case 1:
+          answer += inputNumber;
+          break;
+        case 0:
+          answer -= inputNumber;
+      }
+    }
+    switch (ch) {
+      case '+':
+        isMinus = 1;
+        inputNumber = 0;
+        break;
+      case '-':
+        isMinus = 0;
+        inputNumber = 0;
+        break;
+
+      default:
+        break;
+    }
+  }
+  printf("answer: %d\n", answer);
   return 0;
 }
